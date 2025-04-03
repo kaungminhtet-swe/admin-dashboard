27c8b93eb375845436e87c9bb9af3cdd6d029a97
Kaung Min Htet <kgminhtet.dev@gmail.com>
2025-04-03 16:46:27 +0700

    feat: raw dashboard frame

--- app/dashboard/[account_id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Dashboard() {
-  return (
-    <div className="min-h-full flex justify-center items-center">
-      <Button variant={"outline"}>Welcome To Admin Dashboard</Button>
-    </div>
-  );
-}
